1b9e97a09061d9a1ff5aace1c2056d48decd5aa6
Nikita Ermakov <sh1r4s3@mail.si-head.nl>
2021-03-04 22:06:10 +0300

    Let's try to use main() everywhere

--- kaon/main.c
+++ kaon/main.c
@@ -6,10 +6,10 @@
 
 #include "os/os.h"
 
-ENTRY_POINT
+int main(int argc, char **argv)
 {
     os_create_window("kaon", 800, 600);
     os_loop();
     os_kill_window();
-    return ENTRY_GOOD_RETURN_VALUE;
+    return 0;
 }
--- kaon/os/os.h
+++ kaon/os/os.h
@@ -4,18 +4,6 @@
  * SPDX-License-Identifier: MIT
  */
 
-#ifdef __linux__
-# define ENTRY_GOOD_RETURN_VALUE 0
-# define ENTRY_RETURN_TYPE int
-# define ENTRY_TYPEARGS int argc, const char **argv
-# define ENTRY_ARGS argc, argv
-# define ENTRY_FUNCNAME main
-
-# define ENTRY_POINT \
-    ENTRY_RETURN_TYPE \
-    ENTRY_FUNCNAME(ENTRY_TYPEARGS)
-#endif
-
 struct os_window
 {
     int width, height;
