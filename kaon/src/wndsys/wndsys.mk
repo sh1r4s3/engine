c731c6f85c01edb9e7c361a9bbc3dff3ca7e8c55
Nikita Ermakov <sh1r4s3@mail.si-head.nl>
2021-08-13 03:03:04 +0300

    main: wndsys: Update makefiles

--- kaon/src/wndsys/wndsys.mk
+++ kaon/src/wndsys/wndsys.mk
@@ -1,9 +1,9 @@
-SRC := wndsys.cpp xlib/xlib.cpp
-CFLAGS := -fpic
+SRC := wndsys.cpp xlib/xlib.cpp render/render.cpp render/gl/RenderBackend.cpp
+CXXFLAGS := -fpic
 
 libwndsys.so: $(SRC:.cpp=.o)
 	mkdir -p $(OUTDIR)
-	$(CC) -shared -o $(OUTDIR)/$@ $^ -I$(TOPSRCDIR) -L$(OUTDIR) -lX11 -lGL -lGLU
+	$(CC) -shared $(CXXFLAGS) -o $(OUTDIR)/$@ $^ -I$(TOPSRCDIR) -L$(OUTDIR) -lX11 -lGL -lGLU
 
 clean:
-	rm -vf wndsys.o xlib/xlib.o
+	find . -name "*.o" -delete
