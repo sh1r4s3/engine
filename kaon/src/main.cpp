sh1r4s3/engine

commit a298f6a80757719f23d9031342c58ef652b188de

main: Handle a few events

kaon/src/main.cpp
@@ -8,10 +8,19 @@
 
 int main(int argc, char **argv)
 {
+  bool run = true;
   {
     WndSys wndsys(800, 600, "kaon");
-    while (1) {
-      WndSysEvets::events ev = wndsys.GetEvent();
+    while (run) {
+      WndSysEvents::event ev = wndsys.GetEvent();
+      switch (ev) {
+      case WndSysEvents::draw:
+        wndsys.Draw();
+        break;
+      case WndSysEvents::keyPress:
+        run = true;
+        break;
+      }
     }
   }
 

commit 4afd994825f4d228ffa5195ee7a8d006d2f80931

main: Set the run variable to false to exit the loop
Fixes: a298f6a80757719f23d9031342c58ef652b188de

kaon/src/main.cpp
@@ -18,7 +18,7 @@ int main(int argc, char **argv)
         wndsys.Draw();
         break;
       case WndSysEvents::keyPress:
-        run = true;
+        run = false;
         break;
       }
     }